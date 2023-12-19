385007f62a48b8b40203300e98448153a6a09804
Imran <md.imransyam1956@gmail.com>
2023-12-19 22:23:26 +0600

    fixing env file

--- app/(dashboard)/[storeId]/(routes)/orders/components/OrderClient.tsx
+++ app/(dashboard)/[storeId]/(routes)/orders/components/OrderClient.tsx
@@ -9,7 +9,6 @@ import { Plus } from 'lucide-react'
 import { Separator } from '@/components/ui/separator'
 import { OrderColumn, columns } from './columns'
 import { DataTable } from '@/components/ui/data-table'
-import ApiList from '@/components/ui/api-list'
 
 interface OrderClientInterface{
   data: OrderColumn[]
